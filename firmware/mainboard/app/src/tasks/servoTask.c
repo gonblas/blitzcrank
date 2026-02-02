0852db372b28fb48d33a7bcd688d085769662db3
Mateo Romero <materomero04@gmail.com>
2026-02-02 17:15:53 -0300

    servo deadband update

--- firmware/mainboard/app/src/tasks/servoTask.c
+++ firmware/mainboard/app/src/tasks/servoTask.c
@@ -11,11 +11,18 @@ void servoTask(void* pvParameters) {
   servoConfig(0, SERVO_ENABLE);
   servoConfig(SERVO_PIN, SERVO_ENABLE_OUTPUT);
   uint32_t angle = 0;
+  uint32_t lastAngle = 0;
+  const uint8_t SERVO_DEADBAND = 3; // Only update if angle changes by ≥3 degrees
+  
   for (;;) {
     if (xTaskNotifyWait(0, 0, &angle, portMAX_DELAY) == pdTRUE) {
-        servoWrite(SERVO_PIN, angle);
+        // Only write if angle changed significantly
+        if (abs((int)angle - (int)lastAngle) >= SERVO_DEADBAND) {
+            servoWrite(SERVO_PIN, angle);
+            lastAngle = angle;
+        }
     }
 
-    vTaskDelay(pdMS_TO_TICKS(20));
+    vTaskDelay(pdMS_TO_TICKS(100)); // Increased from 20ms to 50ms to reduce servo stress
   }
 }
